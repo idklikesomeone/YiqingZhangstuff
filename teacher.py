7e501a68fbb199affc8f3c7ca08794a7b158a93d
idklikesomeone <zhangyiqingpurple@gmail.com>
2021-10-26 09:22:36 +1100

    save

---
+++ teacher.py
@@ -0,0 +1,15 @@
+
+
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    04GoodTeacher                               |")
+print("|    Name : Yiqing                               |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+
+teachername = input("Teacher's name (try Mr Horan)")
+if teachername =="Mr Horan":
+    print("You are lucky, he is a great teacher.") 
+else:
+    print(teachername, "is an ok teacher")
