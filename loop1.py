58147932cdf81373d76a2bf3f5c996aad71b3c05
idklikesomeone <zhangyiqingpurple@gmail.com>
2021-10-26 09:33:56 +1100

    save

---
+++ loop1.py
@@ -0,0 +1,10 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    05forLoop                                   |")
+print("|    Name : Yiqing                               |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+
+for x in range (1,500):
+  print(x) 
